08eb55a777cf60fb211bba06e959d20946690671
Sayan Mondal <sayan.mondal4557@gmail.com>
2024-10-19 01:28:34 +0530

    inintial commit

---
+++ src/test/java/com/sayan/enotes/ENotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.enotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
